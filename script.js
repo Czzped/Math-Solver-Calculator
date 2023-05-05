1b654bf8f2555ceb243d2cea4d48ef30454cacc1
Pedro Henrique <phcosta069@gmail.com>
2023-05-05 20:16:07 -0300

    feat: Calculator are now usable

---
+++ script.js
@@ -0,0 +1,49 @@
+const inputArea = document.getElementById('numbersInputContant')
+const clearButton = document.getElementById('clearButton')
+const calculateButton = document.getElementById('calculateButton')
+const allowedKeys = ['1', '2','3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '/', '*']
+
+inputArea.addEventListener('keydown', function (ev) {
+
+    ev.preventDefault()
+    if(allowedKeys.includes(ev.key)){
+
+        inputArea.value += ev.key 
+    }
+    if(ev.key === 'Backspace'){
+
+        inputArea.value = inputArea.value.slice(0, -1)
+    }
+    if(ev.key === 'Enter'){
+
+        calculate()
+    }
+})
+
+calculateButton.addEventListener('click', calculate)
+
+clearButton.addEventListener('click', function allowedKeysFunction() {
+
+    inputArea.value = null
+})
+
+document.querySelectorAll('.sectionsButtonsContant').forEach(function (ev) {
+
+    ev.addEventListener('click', function () {
+
+        inputArea.value += ev.dataset.value
+    })
+})
+
+function calculate() {
+
+    const result = eval(inputArea.value)
+    inputArea.value = result
+}
+
+
+
+
+
+
+
