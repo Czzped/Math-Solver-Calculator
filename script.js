4c803b0b05eaca4a9a72195d8600043085b600e5
Pedro Henrique <phcosta069@gmail.com>
2023-05-05 22:09:07 -0300

    feat: theme button included

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const inputArea = document.getElementById('numbersInputContant')
+const root = document.querySelector(':root')
+const changeThemeButton = document.getElementById('changeThemeButton')
 const copyButton = document.getElementById('copyButton')
 const clearButton = document.getElementById('clearButton')
 const calculateButton = document.getElementById('calculateButton')
 const allowedKeys = ['1', '2','3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '/', '*']
+
+document.querySelectorAll('.sectionsButtonsContant').forEach(function (ev) {
+
+    ev.addEventListener('click', function () {
+
+        inputArea.value += ev.dataset.value
+    })
+})
 
 inputArea.addEventListener('keydown', function (ev) {
 
@@ -21,6 +31,27 @@ inputArea.addEventListener('keydown', function (ev) {
     }
 })
 
+changeThemeButton.addEventListener('click', function () {
+
+    if(changeThemeButton.value == 'Green Theme'){
+
+        changeThemeButton.value = 'Black Theme'
+        root.style.setProperty('---bg-color', '#000000')
+        root.style.setProperty('---color', '#fff')
+
+    }else if(changeThemeButton.value == 'Black Theme'){
+
+        changeThemeButton.value = 'White Theme'
+        root.style.setProperty('---bg-color', '#fff')
+        root.style.setProperty('---color', '#000000')
+    }else if(changeThemeButton.value == 'White Theme'){
+
+        changeThemeButton.value = 'Green Theme'
+        root.style.setProperty('---bg-color', '#79e479')
+        root.style.setProperty('---color', '#fff')
+    }
+})
+
 copyButton.addEventListener('click', function (ev) {
 
     navigator.clipboard.writeText(inputArea.value)
@@ -31,14 +62,6 @@ calculateButton.addEventListener('click', calculate)
 clearButton.addEventListener('click', function allowedKeysFunction() {
 
     inputArea.value = null
-})
-
-document.querySelectorAll('.sectionsButtonsContant').forEach(function (ev) {
-
-    ev.addEventListener('click', function () {
-
-        inputArea.value += ev.dataset.value
-    })
 })
 
 function calculate() {
